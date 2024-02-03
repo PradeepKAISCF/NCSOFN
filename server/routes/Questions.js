053d64241e7baed11352549ed73c41176acbee56
PradeepKAISCF <pradeep123kaiscf@gmail.com>
2024-02-03 11:56:06 +0530

    DONE

--- server/routes/Questions.js
+++ server/routes/Questions.js
@@ -1,18 +1,12 @@
-import express from "express";
+import express from 'express'
+import {AskQuestion, getAllQuestions, deleteQuestion, voteQuestion} from '../controllers/Questions.js'
+import auth from '../middleware/auth.js'
 
-import {
-  AskQuestion,
-  getAllQuestions,
-  deleteQuestion,
-  voteQuestion,
-} from "../controllers/Questions.js";
-import auth from "../middleware/auth.js";
+const router = express.Router()
 
-const router = express.Router();
+router.post('/Ask',AskQuestion)
+router.get('/get',getAllQuestions)
+router.delete('/delete/:id',deleteQuestion)
+router.patch('/vote/:id',voteQuestion)
 
-router.post("/Ask", auth, AskQuestion);
-router.get("/get", getAllQuestions);
-router.delete("/delete/:id", auth, deleteQuestion);
-router.patch("/vote/:id", auth, voteQuestion);
-
-export default router;
+export default router
