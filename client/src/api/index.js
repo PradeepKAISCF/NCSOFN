2b19cd32c02d06d52b61f0000647d6703af714e8
PradeepKAISCF <129294519+PradeepKAISCF@users.noreply.github.com>
2024-02-03 12:04:03 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://stack-overflow-eight.vercel.app/",
+  baseURL: "https://project-null-api2.vercel.app/",
 });
 
 API.interceptors.request.use((req) => {
