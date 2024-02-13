e97d3cdd4251a1565bd1ec7c61ceb4dfbb791b31
PradeepKAISCF <129294519+PradeepKAISCF@users.noreply.github.com>
2024-02-13 23:41:38 +0530

    Update Subscribe.jsx

--- client/src/Pages/Subscribe/Subscribe.jsx
+++ client/src/Pages/Subscribe/Subscribe.jsx
@@ -9,6 +9,7 @@ const Subscribe = ({ slideIn, handleSlideIn }) => {
 
     const [plan,setplan] = useState(null);
     const User = useSelector((state) => state.currentUserReducer);
+    const cUser = useSelector((state) => state.UserReducer);
     const dispatch = useDispatch();
     console.log(User)
     console.log(User.result.subscription)
